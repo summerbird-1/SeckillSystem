1698239dfe3d5285271309ca75790d4a2978ad97
summerbird <8296697+zhang-junzhao@user.noreply.gitee.com>
2024-01-18 13:46:13 +0800

    初始化

---
+++ serve/seckillServe/src/main/java/com/zjz/seckillserve/SeckillServeApplication.java
@@ -0,0 +1,13 @@
+package com.zjz.seckillserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillServeApplication.class, args);
+    }
+
+}
---
+++ serve/seckillServe/src/test/java/com/zjz/seckillserve/SeckillServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjz.seckillserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
